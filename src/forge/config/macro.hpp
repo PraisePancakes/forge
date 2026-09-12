ce08628e2eb99468ed23b7bde342cc900412c1c5
Arya Sharifai <aryasharifai01@yahoo.com>
2026-09-11 21:06:17 -0400

    Initial commit

---
+++ src/forge/config/macro.hpp
@@ -0,0 +1,11 @@
+#ifndef ENTT_CONFIG_MACRO_H
+#define ENTT_CONFIG_MACRO_H
+
+// NOLINTBEGIN(cppcoreguidelines-macro-usage)
+
+#define FORGE_STR(arg) #arg
+#define FORGE_XSTR(arg) FORGE_STR(arg)
+
+// NOLINTEND(cppcoreguidelines-macro-usage)
+
+#endif
